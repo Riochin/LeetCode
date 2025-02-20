6a47989df07aa49222c68cfb5da5be2678e578ee
りおちん <118029.ichikama@gmail.com>
2025-02-20 21:33:20 +0900

    2/20 M1980(Daily) + 修正

--- 1980/1980.find-unique-binary-string.java
+++ 1980/1980.find-unique-binary-string.java
@@ -7,7 +7,18 @@
 // @lc code=start
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        
+        int n = nums.length;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<n;i++){
+            if(nums[i].charAt(i) == '0' ){
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+
+        return sb.toString();
     }
 }
 // @lc code=end
